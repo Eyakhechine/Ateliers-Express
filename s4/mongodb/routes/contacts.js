d86c4c1375c3c27faa32395bbdfacef427643445
unknown <eya_khechine@hotmail.com>
2023-02-17 16:43:56 +0100

    delete contact

--- s4/mongodb/routes/contacts.js
+++ s4/mongodb/routes/contacts.js
@@ -20,12 +20,18 @@ res.json({message:'hello world'})
           ); 
 
 });
-
+// find contact by id
   router.get("/find/:id", function (req, res, next) {
     Contact.findById(req.params.id, (err, contacts) => {
       res.json(contacts);
     });
   });
-
-
+// delete contact
+router.get("/delete/:id", function (req, res, next) {
+  Contact.findByIdAndDelete(req.params.id, (err, contacts) => {
+    console.log(contacts);
+ 
+    res.redirect("/contacts");
+  });
+});
 module.exports = router;
